cd373770bb9b2270f399452aac6a102b50b25b5e
Alexey <tgh88@mail.ru>
2019-01-07 19:28:19 +0300

    post class + ancestors

---
+++ post.rb
@@ -0,0 +1,29 @@
+class Post
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+    #todo
+  end
+
+  def save
+    file = File.new(file_path)
+    to_strings.each do |item|
+      file.puts(item)
+    end
+    file.close
+
+  end
+
+  def file_path
+    current_path = __dir__
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    current_path + "/" + file_name
+  end
+end
